86b502e97f44af4066609c9f7f6552c847996a6d
EnasVen <prophet0630@outlook.com>
2022-07-14 23:10:13 +0800

    ...

--- router/foodrouter.js
+++ router/foodrouter.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var foodcontroller = require('../controller/foodcontroller');
 
 // router.get('/queryall', empcontroller.queryall);
-router.get('/query', foodcontroller.query);
+router.get('https://enasven.herokuapp.com/food/query', foodcontroller.query);
 router.get('/delete', foodcontroller.delete);
 router.post('/insert', foodcontroller.insert);
 router.post('/update', foodcontroller.update);
