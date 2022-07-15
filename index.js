816fadb947f7cc4db29e89b5e8b8a91a8adce5ba
EnasVen <prophet0630@outlook.com>
2022-07-15 11:50:48 +0800

    ...wtf

--- index.js
+++ index.js
@@ -23,9 +23,9 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 app.use('/food', foodrouter);
 
-// app.get('/', (req, res) => {
-//     res.sendFile('./index.html');
-// });
+app.get('/', (req, res) => {
+    res.sendFile('./index.html');
+});
 
 
 var mysql = require('mysql');
